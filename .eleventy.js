fdb308ac6c52e5fb8fa419283f959cfae0105c96
Mark Kimmerle <mkimmerle@yahoo.com>
2025-08-28 12:33:56 -0400

    First draft of Sass files

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
+const sass = require("sass");
+const path = require('node:path');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setIncludesDirectory("_includes");
   eleventyConfig.addPassthroughCopy("art");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("work");
+
+  eleventyConfig.addTemplateFormats("scss");
+
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath)
+      if (parsed.name !== 'main') return
+      let result = sass.compileString(inputContent, {
+          loadPaths: [parsed.dir || '.', this.config.dir.includes],
+          style: 'compressed',
+        });
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
 }
