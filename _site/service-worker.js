7d3e5b32ab2fd6b70b4fecc972eb11b3c11aaa91
Mark Kimmerle <mkimmerle@yahoo.com>
2026-01-20 16:55:08 -0500

    Bug fixes for service worker cache and manifest.json path

--- _site/service-worker.js
+++ _site/service-worker.js
@@ -14,14 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v012126';
+const PRECACHE = 'precache-v012226';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '/offline/',
   './art/mk_profile_img.webp',
-  './css/main.min.090325.css',
+  './css/main.min.121825.css',
   './css/kadwa-v12-latin-700.woff2',
   './css/merriweather-sans-v27-latin-700.woff2',
   './css/merriweather-sans-v27-latin-regular.woff2'
